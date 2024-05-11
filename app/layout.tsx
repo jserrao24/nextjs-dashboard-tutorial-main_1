f3f3f9531f836f95f8125b698c505eaf30acffa9
Jared Serrao <jared.serrao5@gmail.com>
2024-05-10 17:49:11 -1000

    test

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import '@/app/ui/global.css';
+
+import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The best dashboard in the world (also the next.js tutorial)',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className}`}>{children}</body>
+    </html>
+  );
+}
